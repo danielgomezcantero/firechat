050041b49ceb2ec5765c22088afaf6f76bc4154f
danielgomezcantero <danielgomezcantero@gmail.com>
2020-07-08 21:48:45 -0400

    Curso completo firechat

---
+++ src/app/components/chat/chat.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../../services/chat.service';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styles: []
+})
+export class ChatComponent implements OnInit {
+
+
+  mensaje : string ='';   
+  elemento: any;
+  
+  constructor( public chatService:ChatService ) {
+
+    this.chatService.cargarMensajes().subscribe(() =>{
+      
+      setTimeout(()=>{
+        this.elemento.scrollTop = this.elemento.scrollHeight;
+      },20 )
+    }
+    //    (mensajes:any [] ) =>{ 
+    //   console.log(mensajes);
+    // }
+    );
+
+   }
+
+   ngOnInit( ) {
+     this.elemento= document.getElementById('app-mensajes');
+  }
+
+
+  enviarMensaje(){
+    console.log(this.mensaje);
+
+    if( this.mensaje.length === 0 ){
+      return;
+    }
+
+    this.chatService.agregarMensaje( this.mensaje )
+                    .then(()=>{ console.log('Mensaje enviado con exito!'); this.mensaje=""; })
+                    .catch(( err )=>{ console.log('Error al enviar mensaje', err);})
+            
+
+  }
+
+}
